88d5de86a02af24c9978d3d3b9f111fce260be2c
congmng <139330391+congmng@users.noreply.github.com>
2025-06-10 10:33:31 +0800

    change daping

--- src/views/index/new_daping_2.vue
+++ src/views/index/new_daping_2.vue
@@ -26,7 +26,11 @@ const props = defineProps<{
 
 // Reactive data for nodes and resources
 const nodes = reactive<Node[]>([
-    { title: '总计节点数量', value: (props.cloudnum+props.edgenum+props.devicenum)+'个节点', type: 'total-node' },
+    { 
+        title: '总计节点数量', 
+        value: `${props.cloudnum + props.edgenum + props.devicenum}个节点`, 
+        type: 'total-node' 
+    },
     { title: '云侧节点数量', value: props.cloudnum+'个节点', type: 'cloud-node' },
     { title: '边侧节点数量', value: props.edgenum+'个节点', type: 'edge-node' },
     { title: '端侧节点数量', value: props.devicenum+'个节点', type: 'device-node' }
